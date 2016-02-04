eb2dc50a6c0625c59d74ca2fb3aad560084534c4
eeliz1 <eelizo2@lsu.edu>
2016-02-04 10:09:13 -0600

    Assignment 6
    answers (kind of)

---
+++ answers/eeliz1/task1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Assignment 6, task 1
+This program produces different responses based on the value of the integer x
+
+"""
+
+def main(x):
+	if x<4:
+		import antigravity
+	elif x>8:
+		print("xkcd")
+	else:
+		print("webcomic")
+x=3
+
+
+if __name__ == '__main__':
+	main(x)
